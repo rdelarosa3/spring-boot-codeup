d86050a79c5e6dd99cfc75ce3c8dc0f7170964f2
Robert De la Rosa <rdelarosa39@gmail.com>
2020-11-24 11:39:48 -0600

    inital commit

---
+++ src/main/java/com/codeup/demoproject/controllers/PostController.java
@@ -0,0 +1,30 @@
+package com.codeup.demoproject.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class PostController {
+    @GetMapping("/posts") @ResponseBody
+    public String index(){
+        return "These are the posts";
+    }
+
+    @GetMapping("/posts/{id}") @ResponseBody
+    public String viewPost(@PathVariable int id){
+        return "Here is post "+id;
+    }
+
+    @GetMapping("/posts/create") @ResponseBody
+    public String createPost(){
+        return "Form for new post";
+    }
+
+    @PostMapping("/posts/create") @ResponseBody
+    public String writePost(){
+        return "Sending your post";
+    }
+}
