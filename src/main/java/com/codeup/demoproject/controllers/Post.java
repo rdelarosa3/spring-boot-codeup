6466aca08ee0041406b05e76728f50a352d96dec
Robert De la Rosa <rdelarosa39@gmail.com>
2020-11-30 11:29:55 -0600

    Feat: posts index view and show view complete

---
+++ src/main/java/com/codeup/demoproject/controllers/Post.java
@@ -0,0 +1,37 @@
+package com.codeup.demoproject.controllers;
+
+public class Post {
+    long id;
+    String title;
+    String description;
+
+    public Post(long id, String title, String description) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
--- src/main/java/com/codeup/demoproject/controllers/PostController.java
+++ src/main/java/com/codeup/demoproject/controllers/PostController.java
@@ -1,31 +1,49 @@
 package com.codeup.demoproject.controllers;
 
+import com.github.javafaker.Faker;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 public class PostController {
-    @GetMapping("/posts") @ResponseBody
-    public String index(){
+    @GetMapping("/posts")
+    public String index(Model model){
+        Faker faker = new Faker();
+        List<Post> posts = new ArrayList<>();
+        for(int i = 1; i <= 20; i++){
+            posts.add(new Post(
+                    (long) i,
+                    faker.book().title(),
+                    faker.shakespeare().asYouLikeItQuote()
+            ));
+        }
+        model.addAttribute("posts",posts);
         return "/posts/index";
     }
 
-    @GetMapping("/posts/{id}") @ResponseBody
-    public String viewPost(@PathVariable int id){
-        return "/posts/"+id;
+    @GetMapping("/posts/{id}/{title}/{description}")
+    public String viewPost(@PathVariable long id, @PathVariable String title, @PathVariable String description, Model model ){
+        model.addAttribute("id",id);
+        model.addAttribute("title",title);
+        model.addAttribute("description",description);
+        return "/posts/show";
     }
 
-    @GetMapping("/posts/create") @ResponseBody
+    @GetMapping("/posts/create")
     public String createPost(){
         return "/posts/new";
     }
 
-    @PostMapping("/posts/create") @ResponseBody
-    public String submitPost(){
-        
-        return "posts/new";
+    @PostMapping("/posts/create")
+    public String submitPost(@RequestParam(name ="title") String title, @RequestParam(name ="description") String description, Model model){
+        model.addAttribute("title",title);
+        model.addAttribute("description");
+        return "/posts/show";
     }
 }
